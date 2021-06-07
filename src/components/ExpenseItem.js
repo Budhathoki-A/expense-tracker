a4d3eecb4847755eb5571a87ae82458372ebc6f8
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-06-07 20:18:36 +1000

    styling and props added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import '../index.css';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div className='expense-item__date'>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
       </div>
