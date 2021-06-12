446a58f3889d917d26dc28a1cebbb2eee774b495
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-06-12 11:30:21 +1000

    styling updated

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -6,9 +6,9 @@ function ExpenseDate(props) {
   const year = props.date.getFullYear();
   return (
     <div className='expense-date'>
-      <div className='expense-date__year'>{day}</div>
+      <div className='expense-date__day'>{day}</div>
       <div className='expense-date__month'>{month}</div>
-      <div className='expsense-date__day'>{year}</div>
+      <div className='expsense-date__year'>{year}</div>
     </div>
   );
 }
